ccf4c3b50427036e4bc244c59671d987b522d9e3
Noor Al-huda Hamayel <nooralhudahamayel7@gmail.com>
2025-03-09 16:54:09 +0200

    re strucrure

--- app/features/landing-pages/containers/home/home.tsx
+++ app/features/landing-pages/containers/home/home.tsx
@@ -1,9 +1,11 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 
-export function Welcome() {
+export function Home() {
   return (
-    <main className={`flex items-center justify-center pt-16 pb-4 main-section`}>
+    <main
+      className={`flex items-center justify-center pt-16 pb-4 main-section`}
+    >
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0 sub-section">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
