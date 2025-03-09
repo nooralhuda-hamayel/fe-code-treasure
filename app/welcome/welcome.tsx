f9c00481d44d46aa8a3bfd17558cd8d08d478658
Noor Al-huda Hamayel <nooralhudahamayel7@gmail.com>
2025-03-09 16:16:13 +0200

    update packages

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,10 +1,11 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import styles from './style.scss'
 
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+    <main className={`flex items-center justify-center pt-16 pb-4 main-section`}>
+      <div className="flex-1 flex flex-col items-center gap-16 min-h-0 sub-section">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
             <img
