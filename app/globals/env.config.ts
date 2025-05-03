771993f49569a9be5a353c72fa01ce7e89db88ad
Noor Al-huda Hamayel <nooralhudahamayel7@gmail.com>
2025-05-03 23:22:00 +0300

    dev prod env

--- app/globals/env.config.ts
+++ app/globals/env.config.ts
@@ -1,18 +1,24 @@
 function Config() {
-    switch (process.env.NODE_ENV) {
-        case 'development':
-            return {
-                port: 3000,
-                be_server: 'http://localhost:5000'
-            };
-        case 'production':
-            return {
-                port: 443,
-                be_server: 'https://be-code-treasure.onrender.com'
-            };
-        default:
-            return {};
-    }
-};
+  console.log(process.env.NODE_ENV);
+  switch (process.env.NODE_ENV) {
+    case "development":
+      return {
+        port: 3000,
+        be_server: "http://localhost:5000",
+      };
+    case "devprod":
+      return {
+        port: 3000,
+        be_server: "https://be-code-treasure.onrender.com",
+      };
+    case "production":
+      return {
+        port: 443,
+        be_server: "https://be-code-treasure.onrender.com",
+      };
+    default:
+      return {};
+  }
+}
 
 export default Config();
